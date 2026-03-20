d7c0141688c4447197d13db1ad5c090b5acd8e4e
Sandra2026Ads <sandra.regina5050ata@gmail.com>
2026-03-20 22:38:28 +0000

    Estruturei o cód com cometários para facilitar o entendimento

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,50 @@
-#include <stdio.h>
+#include <stdio.h> 
+  //===CAIXA DE FERRAMENTAS(PRINTF,SCANF)Standard Input Output"(Entrada e Saída Padrão)
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  //===ENTRADA: Caixas de Variaveis "RESERVA"(CHAR,INT,FLOAT)
+  
+ 
+  //===\\PASSO1:abrir as gavetas (declarar as variáveis).
 
-  // Área para entrada de dados
+  char estado1;                           //EX: "A"
+  char codigo1[4];                        //EX: "A1"
+  char nome_da_cidade1[50];               //EX: "são paulo"
+  int populacao1;                         //EX: 1000000
+  float area1;                            //EX: 1221.11 KM²
+  float pib1;                             //EX: 811.38 bilhões de reais
+  int quantidade_de_pontos_turisticos1;   //30
 
-  // Área para exibição dos dados da cidade
 
+  // === PROCESSO: CAIXA DE ENTRADA DE DADOS (SCANF "&"") 
+  
+  // ===\\PASSO2:Um printf para perguntar 
+  // ===\\PASSO3:Um scanf (&) para guardar a resposta na gaveta
+
+printf("Qual o Nome da Cidade?\n");
+scanf("%s", nome_da_cidade1);
+
+
+
+
+
+  // === PROCESSO: CAIXA SAÍDA DE DADOS (PRINTF %d,%f,%c) 
+  // ===Aqui é onde você exibe o resultado final organizado
+
+
+
+  // === SAÍDA FINALIZADO COM SUCESSO/ PROGRAMA EXECUTADO/
+  // === Sinal de Sucesso". 
 return 0;
 } 
+
+
+  
+  
+
+ 
+
+   
+  
